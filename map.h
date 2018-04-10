094d9588fa0269982c974b7e3d24a2b5a09553f3
Artem Andryukhov <mouseartiom@yandex.ru>
2018-04-11 02:49:27 +0300

    added constructor/destructor

--- map.h
+++ map.h
@@ -1,20 +1,22 @@
 #ifndef MAP_H
 #define MAP_H
-/*
-GUID needs to be explored
-initSurfMatrix went to private section
-*/
+//This code uses free-distributed library "EasyBMP-1.06". 
+//Great thanks to the authors of this pretty useful tool.
 
+#include "EasyBMP/EasyBMP.h"
 #include "base.h"
 
-static class mapData {
+class MapData {
 public:
+    MapData(const char*);
+    ~MapData();
+    short GetSurfaceType(coord);
+    void getContent(coord); //here must be Event getContent(coord);
+private:
     int mapWidth;
     int mapHeight;
-    short GetSurface(coord);
-private:
-    short** surfaceMap;
-    bool initSurfMatrix(); //returns FALSE if something went wrong (e.g. file doesn't exist)
+    short** surfaceMatrix;
+    bool initSurfMatrix(const char*); //returns FALSE if something went wrong (e.g. file doesn't exist)
     // *vector<GUID> contentMap; 
     // map<GUID, Event> contentList;
     // map<GUID, SurfaceType> surfaceTypesList;
