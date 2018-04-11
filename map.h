4c6173204eeb29e42cb9169e8fddef6252963323
Artem Andryukhov <mouseartiom@yandex.ru>
2018-04-12 00:31:58 +0300

    added getter and one color

--- map.h
+++ map.h
@@ -1,6 +1,7 @@
 #ifndef MAP_H
 #define MAP_H
 //This code uses free-distributed library "EasyBMP-1.06". 
+//https://sourceforge.net/projects/easybmp/
 //Great thanks to the authors of this pretty useful tool.
 
 #include "EasyBMP/EasyBMP.h"
@@ -10,8 +11,8 @@ class MapData {
 public:
     MapData(const char*);
     ~MapData();
-    short GetSurfaceType(coord);
-    void getContent(coord); //here must be Event getContent(coord);
+    short GetSurfaceType(coord*);
+    void getContent(coord*); //here must be Event getContent(coord);
 private:
     int mapWidth;
     int mapHeight;
