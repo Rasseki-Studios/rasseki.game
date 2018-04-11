4a7eea79295de2faf170a64d9ab2ff87512025a0
Artem Andryukhov <mouseartiom@yandex.ru>
2018-04-12 00:31:58 +0300

    added getter and one color

--- map.cpp
+++ map.cpp
@@ -2,6 +2,8 @@
 
 MapData::MapData(const char* filename) {
     initSurfMatrix(filename);
+    
+    //here would be initialization of a content matrix
 }
 
 MapData::~MapData() {
@@ -9,6 +11,10 @@ MapData::~MapData() {
         delete [] surfaceMatrix[i];
     }
     delete [] surfaceMatrix;
+}
+
+short MapData::GetSurfaceType(coord* point) {
+    return surfaceMatrix[point->x][point->y];
 }
 
 bool MapData::initSurfMatrix(const char* filename) {
@@ -22,8 +28,10 @@ bool MapData::initSurfMatrix(const char* filename) {
     //allocating memory
     int i = 0, j = 0;
     surfaceMatrix = new short* [mapHeight];
+    assert(surfaceMatrix);
     for (i = 0; i < mapHeight; i++) {
-        surfaceMatrix[i] = new short [mapWidth];        
+        surfaceMatrix[i] = new short [mapWidth];   
+        assert(surfaceMatrix[i]);
     }
 
     //row by row scanning of bmp file and transformation colours to surface types
@@ -32,20 +40,23 @@ bool MapData::initSurfMatrix(const char* filename) {
             tempPixel = map.GetPixel(i,j);
             intColor = tempPixel.Red * 1000000 + tempPixel.Green * 1000 + tempPixel.Blue;
             switch (intColor) {
-                case 0:
+                case 0:                     //black
                     surfaceMatrix[i][j] = 0;
                     break;
-                case 255255255:
+                case 255000000:             //red
                     surfaceMatrix[i][j] = 1;
                     break;
-                case 255000000:
+                case 255255000:             //yellow
                     surfaceMatrix[i][j] = 2;
                     break;
-                case 255000:
+                case 255:                   //blue
                     surfaceMatrix[i][j] = 3;
                     break;
-                case 255:
-                    surfaceMatrix[i][j] = 4;
+                case 255000:                //green
+                    surfaceMatrix[i][j] = 4 ;
+                    break;
+                case 255255255:             //white
+                    surfaceMatrix[i][j] = 5;
                     break;
             }
         }
