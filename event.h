7d8b3e3073dfce6c3ffd60397afe0970afd2eac8
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-04-12 19:29:38 +0300

    basic event reading added, base improved

--- event.h
+++ event.h
@@ -3,18 +3,25 @@
 
 #include "base.h"
 
+class EventFactory;
+
 class Event : public Item, public Located {
 public:
     void runEvent();
+    /* test function: */
+    void printEvData();
+
+    friend EventFactory;
 private:
+    Event() = default;
+    Event(str, str, short, coord, short);
+    short radius;
     std::vector<Action> eventActions;
 };
 
 class EventFactory : public IFactory {
 public:
     Event* create();
-// private:
-
 };
 
 #endif  // EVENT
