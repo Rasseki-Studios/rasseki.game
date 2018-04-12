598d7aca2f4e5260b9bc8c662652e02816924d8e
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-04-13 02:52:27 +0300

    EventFabric can now read and use all event jsons

--- event.cpp
+++ event.cpp
@@ -1,15 +1,48 @@
 #include "event.h"
 #include <fstream>
-#include "json.hpp"
+#include "libs/json.hpp"
+#include "vector"
 
 #include <iostream>
 
-using json = nlohmann::json;
+void Action::set(str _sID, str _command, str _objID, str _note, short _duration) { 
+    subjectID = _sID;
+    command = _command;
+    objectID = _objID;
+    diaryNote = _note;
+    duration = _duration;
+}
 
-Event::Event(str _ID, str _name, short _level, coord _coord, short _radius) :
+/* DEBUG_FUNCTION */
+void Action::printActData() {
+    std::cout << "subjectID: " << subjectID << std::endl;
+    std::cout << "command: " << command << std::endl;  // Converts in function in run()
+    std::cout << "objectID: " << objectID << std::endl;  // Item *object in future
+    std::cout << "diaryNote: " << diaryNote << std::endl;
+    std::cout << "duration: " << duration << std::endl;
+}
+
+Event::Event(str _ID, str _name, short _level, 
+    coord _coord, short _radius, std::vector<Action> _actions) :
     Item(_ID, _name, _level),
     Located(_coord),
-    radius(_radius) {
+    radius(_radius),
+    actions(_actions) {
+}
+
+void from_json(const json &j, coord &c) {
+    c.x = j.at("x").get<int>();
+    c.y = j.at("y").get<int>();
+}
+
+void from_json(const json &j, Action &action) {
+    action.set(
+        j.at("subject_id").get<str>(),
+        j.at("command").get<str>(),
+        j.at("object_id").get<str>(),
+        j.at("toDiary").get<str>(),
+        j.at("duration").get<short>()
+    );
 }
 
 Event* EventFactory::create() {
@@ -20,21 +53,22 @@ Event* EventFactory::create() {
         j.at("ID").get<str>(),
         j.at("name").get<str>(),
         j.at("level").get<short>(),
-        coord(1, 2),
-        j.at("radius").get<short>()
+        j.at("coord").get<coord>(),
+        j.at("radius").get<short>(),
+        j.at("actions").get<std::vector<Action>>()
     );
     return ev;
 }
 
+/* DEBUG_FUNCTION */
 void Event::printEvData() {
     std::cout << "ID: " << ID << std::endl;
     std::cout << "name: " << name << std::endl;
     std::cout << "level: " << level << std::endl;
+    std::cout << "coords: " << coordinates.x << ", " << coordinates.y << std::endl;
     std::cout << "radius: " << radius << std::endl;
+    for (int i = 0; i < (int)actions.size(); i++) {
+        actions[i].printActData();
+        std::cout << std::endl;
+    }
 }
-
-// void Event::runEvent() {
-//     for (int i = 0; i < eventActions.size(); i++) {
-//         eventActions[i].act();
-//     }
-// }
