925b369a07f5b55b5de46d3e197c878cdce09fad
Moleque <molecada@yandex.ru>
2018-05-17 20:17:27 +0300

    Change libAdapter.sh

--- compile_lib.sh
+++ compile_lib.sh
@@ -1,6 +1,6 @@
-g++ -g -O2 $1 --std=c++17 -c surface.cpp location.cpp item.cpp event.cpp event_factory.cpp event_parser.cpp artifact.cpp storage.cpp artifact_factory.cpp artifact_parser.cpp mapscanner.cpp movable.cpp units.cpp libs/EasyBMP/EasyBMP.cpp items_data.cpp session_data.cpp -lstdc++fs
+g++ -g -O2 $1 --std=c++17 -c libAdapter.cpp surface.cpp location.cpp item.cpp event.cpp event_factory.cpp event_parser.cpp artifact.cpp storage.cpp artifact_factory.cpp artifact_parser.cpp mapscanner.cpp movable.cpp units.cpp libs/EasyBMP/EasyBMP.cpp items_data.cpp session_data.cpp -lstdc++fs
 
-ar rcs rasseki_lib.a surface.o location.o item.o event.o event_factory.o event_parser.o artifact.o storage.o artifact_factory.o artifact_parser.o mapscanner.o movable.o units.o EasyBMP.o items_data.o session_data.o
+ar rcs libAdapter.a libAdapter.o surface.o location.o item.o event.o event_factory.o event_parser.o artifact.o storage.o artifact_factory.o artifact_parser.o mapscanner.o movable.o units.o EasyBMP.o items_data.o session_data.o
 
-rm surface.o location.o item.o event.o event_factory.o event_parser.o artifact.o storage.o artifact_factory.o artifact_parser.o mapscanner.o movable.o units.o EasyBMP.o items_data.o session_data.o
+rm libAdapter.o surface.o location.o item.o event.o event_factory.o event_parser.o artifact.o storage.o artifact_factory.o artifact_parser.o mapscanner.o movable.o units.o EasyBMP.o items_data.o session_data.o
 
--- libAdapter.h
+++ libAdapter.h
@@ -5,11 +5,15 @@
 #include <string>
 
 #include "movable.h"
+#include "session_data.h"
 
 int Game();
+void Exit();
+void Moving(int);
 bool Save(std::string);
 void Load(std::string);
 std::vector<std::string> GetSavedNames();
-bool Go(int x, int y);
+int Go(int x, int y);
+coord Coords();
 
 #endif //LIBADAPTER_H
