87d2abb3705ec81a6a9defec3ec5988dbaf4f6e6
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-05-26 11:14:47 +0300

    removed compile.sh

--- compile.sh
+++
@@ -1,2 +0,0 @@
-g++ -g $1  surface.cpp location.cpp item.cpp event.cpp event_factory.cpp event_parser.cpp artifact.cpp storage.cpp artifact_factory.cpp artifact_parser.cpp mapscanner.cpp movable.cpp hero.cpp libs/EasyBMP/EasyBMP.cpp items_data.cpp session_data.cpp main.cpp -lstdc++fs
-
