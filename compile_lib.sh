8dbafa39de4a808338a1b4b47c38deb133ed91d5
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-05-26 10:45:22 +0300

    resources chaos destroyed

--- compile_lib.sh
+++
@@ -1,6 +0,0 @@
-g++ -g -O2 $1 --std=c++17 -c libAdapter.cpp matrix.cpp surface.cpp location.cpp item.cpp event.cpp event_factory.cpp event_parser.cpp artifact.cpp storage.cpp artifact_factory.cpp artifact_parser.cpp mapscanner.cpp movable.cpp hero.cpp libs/EasyBMP/EasyBMP.cpp items_data.cpp session_data.cpp -lstdc++fs
-
-ar rcs libAdapter.a matrix.o libAdapter.o surface.o location.o item.o event.o event_factory.o event_parser.o artifact.o storage.o artifact_factory.o artifact_parser.o mapscanner.o movable.o hero.o EasyBMP.o items_data.o session_data.o
-
-rm libAdapter.o matrix.o surface.o location.o item.o event.o event_factory.o event_parser.o artifact.o storage.o artifact_factory.o artifact_parser.o mapscanner.o movable.o hero.o EasyBMP.o items_data.o session_data.o
-
