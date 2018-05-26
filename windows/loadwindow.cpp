a638e5e792206dbff0824a69a3f107267125aaa4
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-05-26 12:44:09 +0300

    window files added

---
+++ windows/loadwindow.cpp
@@ -0,0 +1,37 @@
+#include "loadwindow.h"
+#include "ui_loadwindow.h"
+
+LoadWindow::LoadWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::LoadWindow)
+{
+    ui->setupUi(this);
+
+    //установка фона и размеров окна загрузки игры
+    QImage bg_window(":/bground.png");
+    setFixedSize(width(), height());
+    QBrush br;
+    br.setTextureImage(bg_window);
+    QPalette plt = palette();
+    plt.setBrush(QPalette::Background, br);
+    setPalette(plt);
+
+    ui->listWidget->addItem("save1");
+    ui->listWidget->addItem("save2");
+}
+
+LoadWindow::~LoadWindow()
+{
+    delete ui;
+}
+
+void LoadWindow::on_loadLoading_2_clicked()
+{
+
+}
+
+void LoadWindow::on_backLoading_2_clicked()
+{   //закрыть окно загрузки игры
+//    parentWidget()->show();
+    close();
+}
