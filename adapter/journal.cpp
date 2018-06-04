499c9b17581bfa67085da298865b7ae88204d6df
Artem Andryukhov <mouseartiom@yandex.ru>
2018-06-04 22:02:48 +0300

    cosmetic changes

--- adapter/journal.cpp
+++ adapter/journal.cpp
@@ -6,11 +6,11 @@
 
 Journal::Journal(QWidget *parent) : QTextEdit(parent)
 {
-    qDebug() << "hi";
     //connect(this, &Journal::passLogBookEntry, this, &Journal::append);
 
     timer = new QTimer();   //инициализируем Таймер
-    connect(timer, SIGNAL(timeout()), this, SLOT(slotWrite()));    //подключаем СЛОТ для отрисовки к таймеру
+    //подключаем СЛОТ для отрисовки к таймеру
+    connect(timer, SIGNAL(timeout()), this, SLOT(slotWrite()));
     timer->start(50);   //стартуем таймер на 50 миллисекунд
 }
 
