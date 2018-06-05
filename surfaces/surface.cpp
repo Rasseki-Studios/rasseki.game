3677ed862def20c1f824e25a0fd3880bf3d9fb75
Artem Andryukhov <mouseartiom@yandex.ru>
2018-06-05 19:27:33 +0300

    removed ballast code

--- surfaces/surface.cpp
+++ surfaces/surface.cpp
@@ -71,38 +71,3 @@ bool SurfaceData::IsWalkable(Coord point) {
 const Matrix<char>& SurfaceData::getMap() {
     return surfaceMatrix;
 }
-
-// DO NOT use this! It's full of bugs
-AccessMap::AccessMap(Coord start, Coord end) {
-    // start.x--; end.y++;
-    width = abs(start.y - end.y);
-    height = abs(start.x - end.x);
-
-    int frame = 2; // hardcoded for a while
-    int k = 0, n = 0;
-    matrix = new char* [height];
-    
-    for (int i = 0; i < height; i++) { 
-        matrix[i] = new char [width];
-    }
-
-    for (int i = start.x; i < end.x; i++) {
-        for (int j = start.y; j < end.y; j++) {
-            if (surfaceData.CoordIsValid( {i, j} )) {
-                if (surfaceData.getSurface( (Coord){i, j} ) != 0) matrix[n][k] = 1;
-            else matrix[k][n] = 0;                
-            }
-            else matrix[k][n] = 0;
-            n++;
-        }
-        n = 0;
-        k++;
-    }
-}
-
-AccessMap::~AccessMap() {
-    for (int i = 0; i < height; i++) {
-        delete [] matrix[i];
-    }
-    delete [] matrix;
-}
