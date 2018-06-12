056df50eee5c4133533b79297408d778eacc37df
Artem Andryukhov <mouseartiom@yandex.ru>
2018-06-13 02:22:36 +0300

    hardcode moved to config

--- configs/database_config.h
+++ configs/database_config.h
@@ -4,5 +4,8 @@
 const int definedMapHeight = 1000;
 const int definedMapWidth = 1000;
 
+const str defaultLocation = "default_location";
+const str mapName = "map.bmp";
+const str resourcesDirectory = "resources";
 
 #endif
