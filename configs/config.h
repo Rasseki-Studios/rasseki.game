f4c316f2b5199d9e52ed59dc0294b49ea7c806a2
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-06-14 00:42:05 +0300

    compilation error fixed

--- configs/config.h
+++ configs/config.h
@@ -8,4 +8,7 @@ const int timer_delay = 50;
 const int hero_icon_size = 50;
 const int hero_offset = hero_icon_size/2;
 
+const int definedMapHeight = 1000;
+const int definedMapWidth = 1000;
+
 #endif  // CONFIGURATION
